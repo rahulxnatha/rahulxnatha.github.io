5a843d5cc979e7728d49fceb456cca85e3cf6a0b
Rahul Natha <67892338+natharahul@users.noreply.github.com>
2020-11-29 19:52:33 +0530

    Update dark_theme_script.js

--- dark_theme_script.js
+++ dark_theme_script.js
@@ -3,7 +3,26 @@ function manual_dark() {
     var dark_switch_position = document.getElementById("dark-toggle");
       
         element.classList.toggle("dark-mode"); 
+    var logo_src = document.getElementById("logo-on-navbar").src;
+    
+    if (logo_src == "https://docs.google.com/drawings/d/e/2PACX-1vQPdPVytZYNWzSO3PBcLMITfizdMxriBmgDjdtoMztim45F5iTTiJUOJW0cymT7aK8BY6eUcBgbAdFK/pub?w=5369&amp;h=811")
+        {logo_src = "https://docs.google.com/drawings/d/e/2PACX-1vQA4iiyPJ8Py6UjOW524b3qVO8LRSsglJVuLPbldYkJ9td08uvFLAWs1sGukrMnTgmdvkiBCbTap_jD/pub?w=538&amp;h=81"}
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     function automatic_dark_switch(){
         
@@ -13,6 +32,7 @@ function manual_dark() {
   if (hour > 16) {   var toggle_now = document.getElementById("dark-toggle");
                  document.getElementById("tell-user-about-theme").innerHTML = "Good evening. I've turned on dark theme."  ;
                  toggle_now.checked = true; 
+                document.getElementById("logo-on-navbar").src = "https://docs.google.com/drawings/d/e/2PACX-1vQPdPVytZYNWzSO3PBcLMITfizdMxriBmgDjdtoMztim45F5iTTiJUOJW0cymT7aK8BY6eUcBgbAdFK/pub?w=5369&amp;h=811";
     
   } else { if (hour > 11){document.getElementById("tell-user-about-theme").innerHTML = "Good afternoon." ;}else {document.getElementById("tell-user-about-theme").innerHTML = "Good morning.";}
       
@@ -24,18 +44,7 @@ function manual_dark() {
   } else { 
   }
         
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.getElementById("sub-menu1").style.display = "none"; 
-    document.getElementById("splash_screen").style.display = "none";
-    document.getElementById("card1").style.display = "block"; 
-    document.getElementById("card2").style.display = "block";
-    document.getElementById("card3").style.display = "none";
-    document.getElementById("card4").style.display = "none";
-    document.getElementById("card5").style.display = "none"; 
-    document.getElementById("card6").style.display = "none";
-    document.getElementById("card7").style.display = "none";
-    document.getElementById("card8").style.display = "none";  
+           
         
         
         
