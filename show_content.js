5886b8fd1ef7def10787fe3f55f754874919d675
Rahul Natha <67892338+natharahul@users.noreply.github.com>
2021-02-03 14:07:33 +0530

    Update show_content.js

--- show_content.js
+++ show_content.js
@@ -1,118 +1,22 @@
 
 
+
+
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("site_header").style.top = "0";
-       
-      
-  } else { if(currentScrollPos > 204){
-    document.getElementById("site_header").style.top = "-204px";
-         }
+document.getElementById("low_opacity_branding").style.display = "none";
+
+  } else {
+    if (currentScrollPos > 80) {
+      document.getElementById("site_header").style.top = "-80px";
+        document.getElementById("low_opacity_branding").style.top = "0";
+        document.getElementById("low_opacity_branding").style.display = "block";
+
+    }
   }
   prevScrollpos = currentScrollPos;
 }
 
-
-
-
-function show_home_menu(){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    
-    document.getElementById("sub-menu1").style.display = "none"; 
-    document.getElementById("card1").style.display = "block"; 
-    document.getElementById("card2").style.display = "block";
-    document.getElementById("card3").style.display = "none";
-    document.getElementById("card4").style.display = "none";
-    document.getElementById("card5").style.display = "none"; 
-    document.getElementById("card6").style.display = "none";
-    document.getElementById("card7").style.display = "none";
-    document.getElementById("card8").style.display = "none";
-    document.getElementById("card9").style.display = "none";
-    document.getElementById("card10").style.display = "none";
-}
-
-function show_career_menu(){
-    
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.getElementById("sub-menu1").style.display = "block"; 
-    document.getElementById("card1").style.display = "none"; 
-    document.getElementById("card2").style.display = "none";
-    document.getElementById("card3").style.display = "block";
-    document.getElementById("card4").style.display = "block";
-    document.getElementById("card5").style.display = "none"; 
-    document.getElementById("card6").style.display = "none";
-    document.getElementById("card7").style.display = "none";
-    document.getElementById("card8").style.display = "none";
-    document.getElementById("card9").style.display = "none";
-    document.getElementById("card10").style.display = "none";
-}
-
-
-
-function show_exams_menu(){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.getElementById("sub-menu1").style.display = "block"; 
-    document.getElementById("card1").style.display = "none"; 
-    document.getElementById("card2").style.display = "none";
-    document.getElementById("card3").style.display = "none";
-    document.getElementById("card4").style.display = "none";
-    document.getElementById("card5").style.display = "block"; 
-    document.getElementById("card6").style.display = "none";
-    document.getElementById("card7").style.display = "none";
-    document.getElementById("card8").style.display = "none";
-    document.getElementById("card9").style.display = "none";
-    document.getElementById("card10").style.display = "none";
-}
-
-function show_private_jobs_menu(){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.getElementById("sub-menu1").style.display = "block"; 
-    document.getElementById("card1").style.display = "none"; 
-    document.getElementById("card2").style.display = "none";
-    document.getElementById("card3").style.display = "none";
-    document.getElementById("card4").style.display = "none";
-    document.getElementById("card5").style.display = "none"; 
-    document.getElementById("card6").style.display = "block";
-    document.getElementById("card7").style.display = "none";
-    document.getElementById("card8").style.display = "none";
-    document.getElementById("card9").style.display = "none";
-    document.getElementById("card10").style.display = "none";
-}
-
-function show_study_abroad_menu(){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.getElementById("sub-menu1").style.display = "block"; 
-    document.getElementById("card1").style.display = "none"; 
-    document.getElementById("card2").style.display = "none";
-    document.getElementById("card3").style.display = "none";
-    document.getElementById("card4").style.display = "none";
-    document.getElementById("card5").style.display = "none"; 
-    document.getElementById("card6").style.display = "none";
-    document.getElementById("card7").style.display = "block";
-    document.getElementById("card8").style.display = "block";
-    document.getElementById("card9").style.display = "block";
-    document.getElementById("card10").style.display = "none";
-}
-
-function show_study_in_India(){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.getElementById("sub-menu1").style.display = "block"; 
-    document.getElementById("card1").style.display = "none"; 
-    document.getElementById("card2").style.display = "none";
-    document.getElementById("card3").style.display = "none";
-    document.getElementById("card4").style.display = "none";
-    document.getElementById("card5").style.display = "none"; 
-    document.getElementById("card6").style.display = "none";
-    document.getElementById("card7").style.display = "none";
-    document.getElementById("card8").style.display = "none";
-    document.getElementById("card9").style.display = "none";
-    document.getElementById("card10").style.display = "block";
-}
