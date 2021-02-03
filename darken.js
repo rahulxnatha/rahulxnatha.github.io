1d869037bc1a526faefb5db5587b76c043211f49
Rahul Natha <67892338+natharahul@users.noreply.github.com>
2021-02-03 21:13:18 +0530

    Update darken.js

--- darken.js
+++ darken.js
@@ -3,24 +3,47 @@ function automatic_dark_switch() {
     var hour = new Date().getHours();
 
     if (hour > 16) {
-        document.getElementById("tell-user-about-theme").innerHTML = "Good evening. I've turned on dark theme.";
+
+        document.getElementById("tell-user-about-theme").innerHTML = "Good evening. Dark theme is on.";
 
     } else {
-        if (hour > 11) { document.getElementById("tell-user-about-theme").innerHTML = "Good afternoon."; } else { document.getElementById("tell-user-about-theme").innerHTML = "Good morning."; }
+        if (hour > 11) { document.getElementById("tell-user-about-theme").innerHTML = "Good afternoon. Dark theme is on."; } else { document.getElementById("tell-user-about-theme").innerHTML = "Good morning. Dark theme is on."; }
 
     }
 
     if (hour > 0) {
-        document.body.classList.toggle("dark-mode");
-        document.getElementById("dark-toggle").checked = true;
+
 
     } else {
     }
+
+
+    document.body.classList.toggle("dark-mode");
+    document.getElementById("dark-toggle").checked = true;
+    setTimeout(function () { document.getElementById("low_opacity_branding").style.display = "block"; }, 0);
+    setTimeout(function () { document.getElementById("low_opacity_branding").style.display = "none"; }, 3000);
+
 
 }
 
 function manual_dark() {
     document.body.classList.toggle("dark-mode");
+    if (document.getElementById("dark-toggle").checked == false) {
+        document.getElementById("tell-user-about-theme").innerHTML = "You have turned off dark theme.";
+
+        if (document.getElementById("setting_button_mode").checked == true) {
+            document.getElementById("settings_on").style.border = "3px solid #000";
+        }
+
+    } else {
+        document.getElementById("tell-user-about-theme").innerHTML = "Oh wow. You have turned on dark theme.";
+
+        if (document.getElementById("setting_button_mode").checked == true) {
+        document.getElementById("settings_on").style.border = "3px solid #fff";
+    }
+
+    }
+
 
 }
 
