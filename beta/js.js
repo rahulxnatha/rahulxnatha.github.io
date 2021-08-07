f387635f5543cc32114fcca256b6e35f3dbd3145
Rahul Natha <67892338+natharahul@users.noreply.github.com>
2021-08-07 20:21:23 +0530

    Add files via upload

--- beta/js.js
+++ beta/js.js
@@ -1,23 +1,12 @@
 var item = document.getElementsByClassName("item");
 
-for (let i = 2; i < item.length; i++) {
+for (let i = 3; i < item.length; i++) {
     item[i].addEventListener("click", coming_soon);
-  }
+}
 
-function coming_soon(){
+function coming_soon() {
     alert("This feature will start working in the future. Release date is not yet confirmed.");
 }
-
-function close_pop_up() {
-    document.getElementById("suggestion_gform_closed").style.display = "block";
-    document.getElementById("suggestion_gform").style.display = "none";
-}
-
-function pop_up_open() {
-    document.getElementById("suggestion_gform_closed").style.display = "none";
-    document.getElementById("suggestion_gform").style.display = "block";
-}
-
 
 function turnonaccess() {
     var keyentered = document.getElementById("accesskey").value;
@@ -34,103 +23,46 @@ function turnonaccess() {
     else {
         document.getElementById("accessreply").innerHTML = "Access denied!";
     }
-
-
 }
 
-document.getElementById("accesskey").addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("accessbutton").click();
-    }
-});
 
-document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 68) {
-        event.preventDefault();
-        document.getElementById("darktoggle").click();
-    }
-});
-
-document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 67) {
-        event.preventDefault();
-        document.getElementById("calendarswitch").click();
-    }
-});
-
-document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 77) {
-        event.preventDefault();
-        document.getElementById("mediaswitch").click();
-    }
-});
-
-document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 70) {
-        event.preventDefault();
-        document.getElementById("hideonclick").click();
-    }
-});
-document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 84) {
-        event.preventDefault();
-        window.open("https://www.t.me/rahulnatha");
-    }
-});
-
-
-document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 72) {
-        event.preventDefault();
-        document.getElementById("mediaswitch").checked = false;
-        document.getElementById("calendarswitch").checked = false;
-        document.getElementById("mediapage").style.display = "none";
-        document.getElementById("sheet").style.display = "block";
-        document.getElementById("sheet2").style.display = "none";
-        document.getElementById("sheet3").style.display = "none";
-
-
-    }
-});
-
-
-
-var hour = new Date().getHours();
-var date_time = new Date();
-document.getElementById("at").innerHTML = date_time;
-
-document.getElementById("dark-toggle").style.display = "block";
-
-
-function onload_tasks() {
-
-    
-
-    if (new Date().getHours() > 16 || new Date().getHours() < 6) {
+function auto() {
+    let hour = new Date().getHours();
+    let date_time = new Date();
+    if (hour > 16 || hour < 6) {
         document.body.classList.toggle("dark");
         document.getElementById("darktoggle").checked = true;
         document.getElementById("g_frame").style.filter = "invert(90%)";
-        
+
+        var welcome_message = document.getElementById("welcome_message");
+        if (hour > 16) {
+
+            if (hour > 22) { welcome_message.innerHTML = "Good night. See you tomorrow"; }
+            else {
+                welcome_message.innerHTML = "Hi. Good evening.";
+            }
+        }
     }
     else {
-        
+        welcome_message.innerHTML = "Hi. Have a nice day.";
         document.getElementById("darktoggle").checked = false;
         document.getElementById("g_frame").style.filter = "invert(0%)";
     }
 
+    document.getElementById("at").innerHTML = date_time;
     document.getElementById("splash_screen").style.display = "none";
+    document.getElementById("dark-toggle").style.display = "block";
 }
 
 function invert() {
 
     document.body.classList.toggle("dark");
     if (document.getElementById("darktoggle").checked == true) {
-    document.getElementById("g_frame").style.filter = "invert(90%)";
-    
+        document.getElementById("g_frame").style.filter = "invert(90%)";
+
     }
     if (document.getElementById("darktoggle").checked == false) {
-    document.getElementById("g_frame").style.filter = "invert(0%)";
+        document.getElementById("g_frame").style.filter = "invert(0%)";
     }
 
 }
@@ -140,7 +72,7 @@ function widen() {
     document.getElementById("widen").style.gridColumnEnd = 4;
     document.getElementById("widen").style.minHeight = "calc(100vh - 154px)";
 
-document.getElementById("more_content").style.visibility = "visible";
+    document.getElementById("more_content").style.visibility = "visible";
 }
 
 
