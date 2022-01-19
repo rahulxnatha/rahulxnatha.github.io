feb148d281672af63952109fb31ced44c2475c66
Rahul Natha <67892338+natharahul@users.noreply.github.com>
2022-01-19 14:34:53 +0530

    Update color_themes.js

--- docs/career-options/beta/color_themes.js
+++ docs/career-options/beta/color_themes.js
@@ -58,6 +58,38 @@ switcher.addEventListener('click', function manual_theme() {
 document.getElementById("splash_screen").style.display = "none";
 
 
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+        return; // Should do nothing if the default action has been cancelled
+    }
+
+    var handled = false;
+    if (
+        event.key.toLowerCase() === "d"
+        && event.altKey
+        
+    ) {
+        // Handle the event with KeyboardEvent.key and set handled true.
+        document.getElementById("dark-toggle").click();
+        handled = true;
+
+    } else if (
+        event.keyCode.toLowerCase() === "d"
+        && event.altKey
+        
+    ) {
+        // Handle the event with KeyboardEvent.keyCode and set handled true.
+        document.getElementById("dark-toggle").click();
+        handled = true;
+    }
+
+    if (handled) {
+        // Suppress "double action" if event handled
+        event.preventDefault();
+    }
+}, true);
+
+
 
 
 
